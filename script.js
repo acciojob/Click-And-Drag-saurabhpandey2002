00f5448c345d8c24895553ec22bbe1acb56cb2f9
Saurabh Pandey <saurabhpandey200200@gmail.com>
2024-05-30 08:10:53 +0530

    ADD2

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // Your code here.
+const item=document.getElementsByClassName("item");
+
+for(let i=0;i<item.length;i++){
+	item[i].setAttribute("draggable","true");
+	item[i].addEventListener('drag',(e)=>{
+	console.log("dragging");
+})
+}
+
+console.log(item);
